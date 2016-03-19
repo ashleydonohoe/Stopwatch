aed1085c099b64efb41deff0aa6a49ee5198425c
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-03-19 00:39:26 -0400

    added start, pause, resume, and end capabilities

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -10,15 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var timerLabel: UILabel!
+    var timer = NSTimer()
+    var time = 0
+    
     func result() {
-        print("1 second has passed")
+        //print("1 second has passed")
+        time += 1
+        timerLabel.text = String(time)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        var timer = NSTimer()
-        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("result"), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,6 +30,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startTimer(sender: UIBarButtonItem) {
+       timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("result"), userInfo: nil, repeats: true)
+    }
 
+    @IBAction func stopTimer(sender: AnyObject) {
+        timer.invalidate()
+        time = 0
+        timerLabel.text = String(time)
+    }
+    @IBAction func pauseTimer(sender: UIBarButtonItem) {
+        timer.invalidate()
+    }
 }
 
